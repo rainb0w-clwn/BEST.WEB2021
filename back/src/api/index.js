519d4374391cf33027c208d66e97b12eeaac0801
DarrrkS <mikhail.anikin15@gmail.com>
2021-03-12 21:02:20 +0300

    product route init

--- back/src/api/index.js
+++ back/src/api/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var user = require('./routes/user');
 var auth = require('./routes/auth');
+var product = require('./routes/product');
 var test = require('./routes/test');
 module.exports = () => {
     const app = express.Router();
     auth(app);
     user(app);
+    product(app);
     test(app);
     return app;
 };
