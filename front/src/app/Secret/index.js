8ba2a9c4df8bb5f71f4cd4c210c69d35cb5ebfdc
sysmu <75303614+sysmu@users.noreply.github.com>
2021-03-14 02:37:50 +0300

    я попробовал сделать protected_routes, поел говна не смог разобраться с парсингом, там 2 ошибки. Вообще не уверен, что я сделал это правильно

--- front/src/app/Secret/index.js
+++ front/src/app/Secret/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route, Redirect,} from 'react-router-dom';
 
 export default class Secret extends Component {
     constructor() {
@@ -8,18 +9,33 @@ export default class Secret extends Component {
             message: 'Loading...'
         }
     }
+
     componentDidMount() {
         //GET message from server using fetch api
         fetch('/api/secret')
             .then(res => res.text())
             .then(res => this.setState({message: res}));
     }
-    render() {
+
+    function SecretRoute({children, ...rest}) {
         return (
-            <div>
-                <h1>Secret</h1>
-                <p>{this.state.message}</p>
-            </div>
-        );
+            <Route {...rest} render={({location}) => {
+                return fakeAuth.isAuthenticated === true
+                    ? children
+                    : <Redirect to={{
+                        pathname: '/login',
+                        state: { from: location }
+                    }} />
+            }}/>
+        )
     }
-}
+};
+// render() {
+    //    return (
+     //       <div>
+     //           <h1>Secret</h1>
+      //          <p>{this.state.message}</p>
+       //     </div>
+       // );
+  //  }
+//}
--- front/src/app/index.js
+++ front/src/app/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Secret from './Secret';
+import login from './login';
 
 export default class App extends Component {
     render() {
@@ -11,10 +12,12 @@ export default class App extends Component {
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/secret">Secret</Link></li>
+                        <li><Link to="/login">login</Link></li>
                     </ul>
                     <Switch>
                         <Route path="/" exact component={Home} />
-                        <Route path="/secret" component={Secret} />
+                        <Route path="/login" exact component={login} />
+                        <SecretRoute path="/secret" component={Secret} />
                     </Switch>
                 </div>
             </BrowserRouter>
